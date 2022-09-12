3106c4a52a6db412bbcd6a5f03d37f2997236f58
luozhiya <luozhiya@petalmail.com>
2022-09-12 21:26:41 +0800

    merge /home/luozhiya/.local/share/nvim/site/pack/packer/start/nvim-cmp/lua/cmp/utils/misc.lua

--- lua/zycore/base/global.lua
+++ lua/zycore/base/global.lua
@@ -6,5 +6,8 @@
 _G.melantha = {
   -- some vim mappings require a mixture of commandline commands and function calls
   -- this table is place to store lua functions to be called in those mappings
-  mappings = {},
+  -- mappings = {},
+
+  -- main
+  -- zycore = {},
 }
