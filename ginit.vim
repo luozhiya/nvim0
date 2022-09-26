6099d957e776a57fe3e1c890d720365cbbf2d922
luozhiya <luozhiya@petalmail.com>
2022-09-27 03:10:09 +0800

    set ff = unix

--- ginit.vim
+++ ginit.vim
@@ -4,7 +4,7 @@ set mouse=a
 " Set Editor Font
 if exists(':GuiFont')
     " Use GuiFont! to ignore font errors
-    GuiFont! Inconsolata Nerd Font Mono:h13
+    " GuiFont! Inconsolata Nerd Font Mono:h13
 endif
 
 " Disable GUI Tabline
--- lua/zycore/colorscheme.lua
+++ lua/zycore/colorscheme.lua
@@ -10,6 +10,24 @@ endtry
 
 -- Font
 -- guifont = "monospace:h17"    -- the font used in graphical neovim application
--- vim.opt['guifont'] = 'inconsolata:h17' -- the font used in graphical neovim application
+-- vim.opt['guifont'] = 'Inconsolata Nerd Font:h17' -- the font used in graphical neovim application
 
-vim.opt['guifont'] = 'Inconsolata Nerd Font:h17' -- the font used in graphical neovim application
+if vim.fn.has('unix') == 1 then
+  local uname = vim.fn.system('uname')
+  if uname == 'Darwin\n' then
+    vim.opt.guifont = 'JetBrains Mono:h11'
+  else
+    vim.opt['guifont'] = 'inconsolata:h17' -- the font used in graphical neovim application
+  end
+else
+  vim.cmd([[
+  if has('win32')
+    autocmd GUIEnter * simalt ~x  " always maximize initial GUI window
+    set guifont=Consolas:h11
+    if has("directx")
+      set renderoptions=type:directx
+    endif
+  endif
+  ]])
+end
+
