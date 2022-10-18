cf3c22d4a2f4690604c8daa7a861ce24258c3f7c
luozhiya <luozhiya@petalmail.com>
2022-10-18 18:48:32 +0800

    O Only

--- lua/zycore/one/close_buffer_nvim.lua
+++ lua/zycore/one/close_buffer_nvim.lua
@@ -10,11 +10,28 @@ require('close_buffers').setup({
   end,
 })
 
+local hardworking = require('zycore.base.hardworking')
+
+local nnoremap = hardworking.nnoremap
+local inoremap = hardworking.inoremap
+local vnoremap = hardworking.vnoremap
+local xnoremap = hardworking.xnoremap
+
+-- local function close_all_except()
+--   vim.cmd([[:lua require('close_buffers').delete({type = 'other'})<CR>]])
+--   vim.cmd([[:redraw]])
+-- end
+
 -- vim.api.nvim_set_keymap('n', '<leader>th', [[<CMD>lua require('close_buffers').delete({type = 'hidden'})<CR>]],
 --   { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<leader>tu', [[<CMD>lua require('close_buffers').delete({type = 'nameless'})<CR>]],
 --   { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<leader>tc', [[<CMD>lua require('close_buffers').delete({type = 'this'})<CR>]],
 --   { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>z', [[<CMD>lua require('close_buffers').delete({type = 'other'})<CR>]],
-  { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<a-e>', [[<CMD>lua require('close_buffers').delete({type = 'other'})<CR>]],
+--   { noremap = true, silent = true })
+
+-- vim.keymap.set('', '<a-e>', close_all_except)
+
+-- nnoremap('<a-e>', '<ESC>:lua require("close_buffers").delete({type="other"})<CR> ')
+-- nnoremap('<a-e>', ':lua require("close_buffers").delete({type="other"}) vim.cmd([[redraw]]) <CR> ')
