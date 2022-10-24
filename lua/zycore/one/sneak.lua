216c88569df005655f8e75543c3d4d098e8edd80
luozhiya <luozhiya@petalmail.com>
2022-10-24 14:00:38 +0800

    no leap

--- lua/zycore/one/sneak.lua
+++ lua/zycore/one/sneak.lua
@@ -1,5 +1,5 @@
 -- 2-character Sneak (default)
 
 require('leap').add_default_mappings()
-require('leap').leap({ target_windows = { vim.fn.win_getid() } })
+-- require('leap').leap({ target_windows = { vim.fn.win_getid() } })
 require('flit').setup({ labeled_modes = 'nv' })
