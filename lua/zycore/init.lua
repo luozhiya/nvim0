3734b74fde64cfaafa26a2ad0fff8697ba9cdb4c
luozhiya <luozhiya@petalmail.com>
2022-11-07 12:00:19 +0800

    refs

--- lua/zycore/init.lua
+++ lua/zycore/init.lua
@@ -1,2 +1,9 @@
 local base = require('zycore.base')
 local features = require('zycore.features')
+
+-- Refs
+-- https://github.com/LunarVim/Neovim-from-scratch
+-- https://github.com/wbthomason/dotfiles/tree/linux/neovim/.config/nvim
+-- https://github.com/akinsho/dotfiles/tree/main/.config/nvim
+-- https://git.sr.ht/~yazdan/nvim-config/tree
+-- 
