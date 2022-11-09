fc8bac849d8ef81d3c90f12064f2dc75ce4f7dca
luozhiya <luozhiya@petalmail.com>
2022-11-09 22:55:03 +0800

    0

--- lua/zycore/one/colorscheme.lua
+++ lua/zycore/one/colorscheme.lua
@@ -96,7 +96,7 @@ endif
 ]])
 
 vim.opt.list = true
+vim.opt.listchars = 'eol:↴'
 -- vim.opt.listchars:append('space:⋅')
 -- vim.opt.listchars:append('space:')
-vim.opt.listchars = 'eol:↴'
 vim.opt.listchars:append('tab:<->')
